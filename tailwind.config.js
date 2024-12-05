800ca28be0021a0d91072c26706f9d85e57488d5
Phobyas <162043716+Phobyas@users.noreply.github.com>
2024-12-05 20:35:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./**/*.liquid", // Ensure Shopify Liquid files are scanned
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+};
